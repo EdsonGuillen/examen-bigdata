10664ea0566bfc853c0a6c1558902d6b69655d7e
Victorml55 <victorml5505@gmail.com>
2026-01-28 21:49:04 -0600

    Update web.php
    se agrego la ruta para la brecha salarial

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@ Route::get('/reportes/managers', [ReportController::class, 'managers']);
 Route::get('/graficos/genero', [ReportController::class, 'graficoGenero']);
 Route::get('/graficos/top-salarios', [ReportController::class, 'graficoTopSalarios']);
 Route::get('/graficos/promedios', [ReportController::class, 'graficoPromedios']);
+Route::get('/graficos/brecha-salarial', [ReportController::class, 'graficoBrechaSalarial']);
 Route::get('/reportes/listado', [ReportController::class, 'listado']);
 Route::get('/reportes/estadisticas', [ReportController::class, 'estadisticasDeptos']);
